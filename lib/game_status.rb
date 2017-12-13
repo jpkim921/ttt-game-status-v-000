e083dd7e5c83ff51a9dd68dcc6c1e4fbcf758c1a
jpkim921 <jpkim921@gmail.com>
2017-12-13 04:01:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,23 +21,12 @@ def won?(board)
 
 board.all?{|ele| ele == " " } == true
 
-WIN_COMBINATIONS.each do |win_combo|
+  WIN_COMBINATIONS.each do |win_combo|
+    position1 = board[win_combo[0]]
+    position2 = board[win_combo[1]]
+    position3 = board[win_combo[2]]
 
 
-
-      win_index_1 = win_combo[0]
-      win_index_2 = win_combo[1]
-      win_index_3 = win_combo[2]
-
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-            (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return win_combo
-      else
-        return false
-      end
-    end
+    if win_combo ==
+  end
 end
