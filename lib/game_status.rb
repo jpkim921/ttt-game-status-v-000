2a8b120ea9ecbfd827a040fad89e49ce5523d3a0
jpkim921 <jpkim921@gmail.com>
2017-12-13 05:41:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,11 +55,15 @@ end
 
 def over?(board)
 
-if draw?(board)
+# if draw?(board)
+#   return true
+# elsif won?(board) || full?(board)
+#   return true
+# end
+
+if won?(board) || full?(board)
   return true
-elsif won?(board) == true || full?(board)
-  return true
-end
+end 
 
 # if won?(board) == true || full?(board) == false
 # return true
