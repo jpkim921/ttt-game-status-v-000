3037368e22233dbe689ce42f68eb0104158a715d
jpkim921 <jpkim921@gmail.com>
2017-12-13 04:03:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,9 +25,9 @@ board.all?{|ele| ele == " " } == true
     position1 = board[win_combo[0]]
     position2 = board[win_combo[1]]
     position3 = board[win_combo[2]]
-
-
-    if [position1, position2, position3] == ["X","X","X"] || [position1, position2, position3] == ["O","O","O"]
+    comparison_set = [position1, position2, position3]
+    
+    if comparison_set == ["X","X","X"] || comparison_set == ["O","O","O"]
       return win_combo
     else
       return false
