cf2123422199d60e05e59ca14368953b171baf46
jpkim921 <jpkim921@gmail.com>
2017-12-13 03:35:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,7 +37,7 @@ def won?(board)
             (position_1 == "O" && position_2 == "O" && position_3 == "O")
         return win_combo
       else
-        false
+        return false
       end
     end
 end
